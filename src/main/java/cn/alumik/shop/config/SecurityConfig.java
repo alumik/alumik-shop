a6408e0dda3500ea545b0aa746cc5648c633b4cb
Zhong Zhenyu <nczzy1997@gmail.com>
2019-06-30 07:06:57 +0800

    添加错误页/完成登陆错误提示

--- src/main/java/cn/alumik/shop/config/SecurityConfig.java
+++ src/main/java/cn/alumik/shop/config/SecurityConfig.java
@@ -15,15 +15,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         User.UserBuilder users = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
-                .withUser(users.username("usr1").password("pwd1").roles("LEVEL_0", "LEVEL_1"))
-                .withUser(users.username("usr2").password("pwd2").roles("LEVEL_2"))
-                .withUser(users.username("usr3").password("pwd3").roles("LEVEL_3"));
+                .withUser(users.username("admin").password("admin").roles("ADMIN"))
+                .withUser(users.username("demo").password("demo").roles("DEMO"));
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/webjars/**", "/css/**").permitAll()
+                .antMatchers("/", "/webjars/**", "/css/**", "/error").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").loginProcessingUrl("/authenticate").permitAll()
