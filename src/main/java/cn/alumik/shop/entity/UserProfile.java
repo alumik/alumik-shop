614db62b87df83f7740275d7823dcba35d3dbb23
Zhong Zhenyu <nczzy1997@gmail.com>
2019-07-01 15:30:36 +0800

    创建 UserProfile 类及 DAO

---
+++ src/main/java/cn/alumik/shop/entity/UserProfile.java
@@ -0,0 +1,115 @@
+package cn.alumik.shop.entity;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "user_profile")
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @OneToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "id_user")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_gender")
+    private Gender gender;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "detail")
+    private String detail;
+
+    @Column(name = "avatar")
+    private String avatar;
+
+    @Column(name = "join_date")
+    private Date joinDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date join_date) {
+        this.joinDate = join_date;
+    }
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "id=" + id +
+                ", user=" + user +
+                ", gender=" + gender +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", detail='" + detail + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", join_date=" + joinDate +
+                '}';
+    }
+}
