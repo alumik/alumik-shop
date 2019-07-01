230750bfbc195c628248cec4bd70b8c677df7389
Zhong Zhenyu <nczzy1997@gmail.com>
2019-07-01 16:19:27 +0800

    创建 Item 类和 DAO

---
+++ src/main/java/cn/alumik/shop/dao/Item.java
@@ -0,0 +1,6 @@
+package cn.alumik.shop.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Item extends JpaRepository<Item, Integer> {
+}
--- src/main/java/cn/alumik/shop/entity/Item.java
+++ src/main/java/cn/alumik/shop/entity/Item.java
@@ -1,13 +1,135 @@
 package cn.alumik.shop.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 @Entity
+@Table(name = "item")
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 
     @ManyToOne
     @JoinColumn(name = "id_category")
     private Category category;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "pic")
+    private String pic;
+
+    @Column(name = "detail")
+    private String detail;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "stock")
+    private int stock;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "seller")
+    private User seller;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "modified_at")
+    private Timestamp modifiedAt;
+
+    @Column(name = "available")
+    private Boolean available;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Timestamp modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
 }
