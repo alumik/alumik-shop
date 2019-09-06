76f688d3f2a6ab69f25f91225afa51790da9a84b
Zhong Zhenyu <nczzy1997@gmail.com>
2019-09-06 18:16:17 +0800

    移动登录与注册功能位置

--- src/main/java/cn/alumik/shop/controller/SiteController.java
+++ src/main/java/cn/alumik/shop/controller/SiteController.java
@@ -1,9 +1,16 @@
 package cn.alumik.shop.controller;
 
+import cn.alumik.shop.entity.User;
 import cn.alumik.shop.service.ItemService;
+import cn.alumik.shop.service.SecurityService;
+import cn.alumik.shop.service.UserService;
+import cn.alumik.shop.validator.UserValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,8 +21,17 @@ public class SiteController {
 
     private ItemService itemService;
 
-    public SiteController(ItemService itemService) {
+    private UserValidator userValidator;
+
+    private UserService userService;
+
+    private SecurityService securityService;
+
+    public SiteController(ItemService itemService, UserValidator userValidator, UserService userService, SecurityService securityService) {
         this.itemService = itemService;
+        this.userValidator = userValidator;
+        this.userService = userService;
+        this.securityService = securityService;
     }
 
     @GetMapping("/")
@@ -28,5 +44,27 @@ public class SiteController {
     @GetMapping("/about")
     public String actionAboutGetter() {
         return "site/about";
+    }
+
+    @GetMapping("/registration")
+    public String actionRegistration(Model model) {
+        model.addAttribute("userForm", new User());
+        return "site/registration";
+    }
+
+    @PostMapping("/registration")
+    public String actionRegistration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+        userValidator.validate(userForm, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "site/registration";
+        }
+        userService.save(userForm, true);
+        securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String actionLogin() {
+        return "site/login";
     }
 }
